078ccac30f7806eb6bafafd2fe91026a573b250c
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-15 16:47:22 +0600

    some projects add

--- db.js
+++ db.js
@@ -1,18 +1,24 @@
 import DaringPage from "./public/projects/daringpage.png";
 import EShop from "./public/projects/eshop.png";
-import EssayContest from "./public/projects/essaycontestapp.jpg";
+import EssayContest from "./public/projects/essaycontestapp.png";
 import PasswordVault from "./public/projects/passwordvault.png";
 import MathPlus from "./public/projects/mathplus.png";
-import Shopmate from "./public/projects/shopmate.jpg";
+import CourseTemplateReact from "./public/projects/course-template-react.png";
+import HealthCareTemplate from "./public/projects/health-care-template.png";
+import Spaymart from "./public/projects/spaymart.png";
+import WeatherWithDom from "./public/projects/weather-with-dom.png";
+import Shopmate from "./public/projects/shopmate.png";
+import KirinAvaturn from "./public/projects/kirin-avaturn.png";
+import Keflemariam from "./public/projects/keflemariam.png";
 import Polling from "./public/projects/polling.jpg";
 import TodoList from "./public/projects/todolist.jpg";
 import EssayContestPage from "./public/projects/essaycontentlp.png";
-import Q2Life from "./public/projects/q2life.png";
+import Q2Life from "./public/projects/q2life.jpg";
+import Dietis from "./public/projects/dietis.png";
 import Cude from "./public/projects/cude.png";
-import Duhoot from "./public/projects/duhoot.png";
-import BingoGame from "./public/projects/bingogame.png";
 import Portfolio1 from "./public/projects/portfolio1.png";
-import Portfolio2 from "./public/projects/portfolio2.png";
+import BlogUsingThirdPartyApi from "./public/projects/blog-using-third-party-api.png";
+import TicTacToe from "./public/projects/tic-tac-toe.png";
 import Alekhya from "./public/projects/alekhya.org.png";
 
 const Projects = [
@@ -29,33 +35,15 @@ const Projects = [
       "Tailwind",
       "Expressjs",
       "MongoDB",
+      "Multer",
+      "Cloudinary",
       "SMTP server",
       "Twilio API",
-      "Netlify",
-      "Cyclic",
+      "Vercel",
     ],
     height: "550",
     githubLink: "https://github.com/mlhnshuvo/multi-vendor-ecommerce",
-    webLink: "https://eshop-multi-vendor.netlify.app",
-  },
-  {
-    id: "DaringPage",
-    type: "fullstack",
-    image: DaringPage,
-    title: "Daring Page",
-    description:
-      "Daring Page is a blog application where users can read and learn about programming languages, libraries, frameworks and trending technologies",
-    technologies: [
-      "Nextjs",
-      "Next-auth",
-      "Tailwind",
-      "Expressjs",
-      "MongoDB",
-      "Vercel",
-    ],
-    height: "190",
-    githubLink: "https://github.com/mlhnshuvo/ecommerce-app",
-    webLink: "https://daringpage.vercel.app",
+    webLink: "https://eshop-multi-vendor.vercel.app",
   },
   {
     id: "PasswordVault",
@@ -75,7 +63,26 @@ const Projects = [
     ],
     height: "480",
     githubLink: "https://github.com/mlhnshuvo/password-manager-app",
-    webLink: "https://password-manager-mhs.netlify.app",
+    webLink: "https://password-vault-app.vercel.app",
+  },
+  {
+    id: "DaringPage",
+    type: "fullstack",
+    image: DaringPage,
+    title: "Daring Page",
+    description:
+      "Daring Page is my blog application with nextjs. It is totally SEO friendly.🚀 I just tried to clean and simple the UI so that user can use it easily. 💻",
+    technologies: [
+      "Nextjs",
+      "Next-auth",
+      "Tailwind",
+      "Expressjs",
+      "MongoDB",
+      "Vercel",
+    ],
+    height: "190",
+    githubLink: "https://github.com/mlhnshuvo/blog-app-nextjs",
+    webLink: "https://daringpage.vercel.app",
   },
   {
     id: "EssayContest",
@@ -92,15 +99,15 @@ const Projects = [
       "MongoDB",
       "SMTP server",
       "Netlify",
-      "Cyclic",
+      "Render",
     ],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/monomousumicontest",
+    height: "",
+    githubLink: "https://github.com/mlhnshuvo/essay-contest-of-students",
     webLink: "https://www.monomousumicontest.com",
   },
   {
     id: "MathPlus",
-    type: "frontend",
+    type: "fullstack",
     image: MathPlus,
     title: "Math Plus",
     description:
@@ -114,9 +121,45 @@ const Projects = [
       "Netlify",
       "Cyclic",
     ],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/monomousumicontest",
-    webLink: "https://www.monomousumicontest.com",
+    height: "130",
+    githubLink: "https://github.com/mlhnshuvo/math-plus-for-kids",
+    webLink: "https://math-plus-app.vercel.app",
+  },
+  {
+    id: "CourseTemplateReact",
+    type: "others",
+    image: CourseTemplateReact,
+    title: "Course Template React",
+    description:
+      "Beautiful and fresh UI react template. I build it for one of my client📚",
+    technologies: ["React", "Tailwind", "Netlify"],
+    height: "570",
+    githubLink: "https://github.com/mlhnshuvo/react-course-template",
+    webLink: "https://course-template-react.netlify.app",
+  },
+  {
+    id: "HealthCareTemplate",
+    type: "others",
+    image: HealthCareTemplate,
+    title: "Health Care Template",
+    description:
+      "💊This is a healthcare template for influencers and companies who want to donate money or something to help poor people who cannot afford treatment💊",
+    technologies: ["React", "Netlify"],
+    height: "830",
+    githubLink: "https://github.com/mlhnshuvo/health-care-template",
+    webLink: "https://health-care-template.netlify.app",
+  },
+  {
+    id: "Spaymart",
+    type: "others",
+    image: Spaymart,
+    title: "Spaymart Animal Saving Template",
+    description:
+      "This design is inspired by SPAYMART with reactjs. SPAYMART is an animal welfare organization established to end the unnecessary killing of animals🐈",
+    technologies: ["React", "Tailwind", "Netlify"],
+    height: "1510",
+    githubLink: "https://github.com/mlhnshuvo/animals-saving-react-template",
+    webLink: "https://spaymart.netlify.app",
   },
   {
     id: "Shopmate",
@@ -124,11 +167,107 @@ const Projects = [
     image: Shopmate,
     title: "Shop Mate",
     description:
-      "Shop Mate is a simple functional-based application where users can add, delete and update buckets and add a list of items under each bucket.",
+      "Shop Mate is a simple functional-based application where users can add, delete and update buckets and add a list of items under each bucket. This application inspired by google🥼",
     technologies: ["React", "BEM", "Easy-peasy", "Netlify"],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/shopmate",
+    height: "",
+    githubLink: "https://github.com/mlhnshuvo/shopping-list-app",
     webLink: "https://shopmate-mhs.netlify.app",
+  },
+
+  {
+    id: "Q2Life",
+    type: "landingpage",
+    image: Q2Life,
+    title: "Q2Life",
+    description:
+      "This is simple but beautiful and clean UI one page business figma to html template with dom manipulation🖥️",
+    technologies: ["Figma", "HTML", "CSS", "DOM", "Netlify"],
+    height: "600",
+    githubLink: "https://github.com/mlhnshuvo/q2life-landing-page",
+    webLink: "https://q2life.netlify.app",
+  },
+  {
+    id: "Dietis",
+    type: "others",
+    image: Dietis,
+    title: "Dietis React Template",
+    description:
+      "This is a design for an organization that was designed by Figma to React component💻",
+    technologies: ["Figma", "React", "Recharts", "Netlify"],
+    height: "80",
+    githubLink: "https://github.com/mlhnshuvo/react-template-dietis",
+    webLink: "https://dietis.netlify.app",
+  },
+  {
+    id: "Cude",
+    type: "landingpage",
+    image: Cude,
+    title: "Cude",
+    description: "Landing page with html, css, jquery plugins 💻",
+    technologies: ["HTML", "CSS", "jQuery", "Netlify"],
+    height: "1000",
+    githubLink: "https://github.com/mlhnshuvo/cude-landing-page",
+    webLink: "https://cude-page.netlify.app",
+  },
+  {
+    id: "Portfolio1",
+    type: "landingpage",
+    image: Portfolio1,
+    title: "Portfolio 1",
+    description:
+      "This is a beautiful HTML, CSS, Bootstrap, jquery plugin portfolio",
+    technologies: ["HTML", "CSS", "Bootstrap", "jQuery", "Netlify"],
+    height: "820",
+    githubLink: "https://github.com/mlhnshuvo/portfolio1",
+    webLink: "https://portfolio1-landing-page.netlify.app",
+  },
+
+  {
+    id: "EssayContestPage",
+    type: "others",
+    image: EssayContestPage,
+    title: "Essay Contest Winners",
+    description:
+      "This is a page for essay contest students. Here they can see Awards, Prizes, Magazines etc.",
+    technologies: ["React", "Netlify", "Render"],
+    height: "1910",
+    githubLink: "https://github.com/mlhnshuvo/essay-contest-winner-page",
+    webLink: "https://essay.monomousumicontest.com",
+  },
+  {
+    id: "KirinAvaturn",
+    type: "others",
+    image: KirinAvaturn,
+    title: "Avatar Builder with Avaturn API",
+    description:
+      "Basically, it's a react template and one of my clients said to add the avaturn API for some interactivity🧞‍♂️",
+    technologies: ["React", "Netlify", "Render"],
+    height: "200",
+    githubLink: "https://github.com/mlhnshuvo/avatar-builder-template",
+    webLink: "https://kirin-avaturn.netlify.app",
+  },
+  {
+    id: "Keflemariam",
+    type: "others",
+    image: Keflemariam,
+    title: "Keflemariam's Portfolio",
+    description:
+      "It was designed from Figma to HTML and then reactjs. Clean and good looking design💻",
+    technologies: ["React", "Netlify", "Figma"],
+    height: "670",
+    githubLink: "https://github.com/mlhnshuvo/client-portfolio",
+    webLink: "https://keflemariam.netlify.app",
+  },
+  {
+    id: "WeatherWithDom",
+    type: "frontend",
+    image: WeatherWithDom,
+    title: "Weather With Dom",
+    description: "A simple weather app with JS AND DOM manipulation ☁",
+    technologies: ["JS", "DOM manipulation", "HTML", "CSS", "Vercel"],
+    height: "",
+    githubLink: "https://github.com/mlhnshuvo/weather-app-with-dom",
+    webLink: "https://weather-with-dom.vercel.app",
   },
   {
     id: "Polling",
@@ -138,9 +277,9 @@ const Projects = [
     description:
       "Polling is a simple functional-based application where users can add, delete and update poll category and add vote under each category.",
     technologies: ["React", "Netlify"],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/poll-app",
-    webLink: "https://polling-mhs.netlify.app",
+    height: "",
+    githubLink: "https://github.com/mlhnshuvo/react-poll-app",
+    webLink: "https://poll-vote.netlify.app",
   },
   {
     id: "TodoList",
@@ -150,89 +289,32 @@ const Projects = [
     description:
       "TodoList is a simple application where users can add and filter todo",
     technologies: ["React", "Netlify"],
-    height: "100",
+    height: "",
     githubLink: "https://todolist-mhs.netlify.app",
     webLink: "https://github.com/mlhnshuvo/todo-app",
   },
   {
-    id: "Q2Life",
-    type: "landingpage",
-    image: Q2Life,
-    title: "Q2Life",
-    description:
-      "Q2Life is a good-looking business web page with some DOM operation.",
-    technologies: ["Figma", "HTML", "CSS", "DOM", "Netlify"],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/q2life-landing-page",
-    webLink: "https://q2life.netlify.app",
-  },
-  {
-    id: "Cude",
-    type: "landingpage",
-    image: Cude,
-    title: "Cude",
-    description: "A simple landing page with html, css and totally responsive.",
-    technologies: ["HTML", "CSS", "jQuery", "Netlify"],
-    height: "1000",
-    githubLink: "https://github.com/mlhnshuvo/cude-landing-page",
-    webLink: "https://cude-mhs.netlify.app",
-  },
-  {
-    id: "Duhoot",
-    type: "landingpage",
-    image: Duhoot,
-    title: "Duhoot",
-    description: "A simple landing page with html, css and totally responsive.",
-    technologies: ["HTML", "CSS", "jQuery", "Netlify"],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/duhoot-landing-page",
-    webLink: "https://duhoot.netlify.app",
-  },
-  {
-    id: "Portfolio1",
-    type: "landingpage",
-    image: Portfolio1,
-    title: "Portfolio 1",
-    description: "This is my first portfolio with totally responsive",
-    technologies: ["HTML", "CSS", "jQuery", "Netlify"],
-    height: "100",
-    githubLink: "https://github.com/mlhnshuvo/duhoot-landing-page",
-    webLink: "https://duhoot.netlify.app",
-  },
-  {
-    id: "BingoGame",
+    id: "TicTacToe",
     type: "others",
-    image: BingoGame,
-    title: "Bingo Game",
-    description:
-      "This is few pages of Bing game. Play bingo live on the best platform in Brazil",
+    image: TicTacToe,
+    title: "Tic Tac Toe Game",
+    description: "React tic tac toe game🏆",
     technologies: ["React", "Netlify"],
-    height: "100",
-    githubLink: "https://portfolio2-mhs.netlify.app",
-    webLink: "https://github.com/mlhnshuvo/portfolio2",
+    height: "",
+    githubLink: "https://github.com/mlhnshuvo/tic-tac-toe-game",
+    webLink: "https://tic-tac-toe-mhs.netlify.app",
   },
   {
-    id: "Portfolio2",
+    id: "BlogUsingThirdPartyApi",
     type: "others",
-    image: Portfolio2,
-    title: "Portfolio2",
-    description: "This is my second portfolio with totally responsive",
-    technologies: ["React", "Netlify"],
-    height: "100",
-    githubLink: "https://portfolio2-mhs.netlify.app",
-    webLink: "https://github.com/mlhnshuvo/portfolio2",
-  },
-  {
-    id: "EssayContestPage",
-    type: "others",
-    image: EssayContestPage,
-    title: "Essay Contest Winners",
+    image: BlogUsingThirdPartyApi,
+    title: "Blog Using Third Party Api",
     description:
-      "This is a page for essay contest students. Here they can see Awards, Prizes, Magazines etc.",
-    technologies: ["React", "Netlify"],
-    height: "1738",
-    githubLink: "https://essay-contest-winner-page.netlify.app",
-    webLink: "https://essay.monomousumicontest.com",
+      "This is a simple blog app using a third-party API service with reactjs and bootstrap",
+    technologies: ["React", "Bootstrap", "API", "Vercel"],
+    height: "480",
+    githubLink: "https://github.com/mlhnshuvo/blog-using-third-party-api",
+    webLink: "https://blog-using-third-party-api.vercel.app",
   },
   {
     id: "Alekhya",
