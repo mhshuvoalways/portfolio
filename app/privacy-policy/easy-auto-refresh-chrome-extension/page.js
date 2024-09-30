d356df018472b6f1a5a6822443d94ef49dc39090
MH Shuvo <mhshuvoalways@gmail.com>
2024-09-30 22:47:05 +0600

    feat: add privacy policy page for auto refresh chrome extension

---
+++ app/privacy-policy/easy-auto-refresh-chrome-extension/page.js
@@ -0,0 +1,93 @@
+import MainComponent from "@/app/components/common/main";
+
+const page = () => {
+  return (
+    <MainComponent>
+      <div div className="mt-10 lg:mt-20 text-gray-200 space-y-10">
+        <div className="space-y-5">
+          <p className="text2">
+            Privacy Policy for Easy Auto Refresh Chrome Extension
+          </p>
+          <div>
+            <p className="text3">1. Introduction</p>
+            <p className="mt-2">{`The Easy Auto Refresh Chrome Extension ("we," "us," or "our") is dedicated to safeguarding your privacy. This Privacy Policy outlines how we collect, use, and share user data when you use our extension.`}</p>
+          </div>
+        </div>
+        <div>
+          <p className="text3">2. Information We Collect</p>
+          <ul className="mt-2 list-disc pl-7">
+            <li>
+              Tab Information: We collect the ID of the specific tab where the
+              auto-refresh feature is initiated. This data helps us manage the
+              auto-refresh functionality effectively.
+            </li>
+            <li>
+              User Preferences: We store your preferences regarding refresh
+              intervals and settings to enhance your user experience.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text3">3. How We Use Your Information</p>
+          <p className="mt-2">
+            We use the collected information for the following purposes:
+          </p>
+          <ul className="mt-2 list-disc pl-7">
+            <li>
+              Functionality: To enable the auto-refresh feature and ensure that
+              your settings are applied correctly.
+            </li>
+            <li>
+              User Experience: To remember your preferences and provide a more
+              personalized experience.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text3">4. Data Sharing</p>
+          <ul className="mt-2 list-disc pl-7">
+            <li>
+              Third Parties: We do not share your data with any third parties.
+              The information collected is stored locally within your browser
+              and is only accessible to the Easy Auto Refresh Chrome Extension.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text3">5. Data Storage</p>
+          <p className="mt-2">
+            {`Your data is stored locally in your browser's storage and is not
+              sent to our servers or shared with any external parties.`}
+          </p>
+        </div>
+        <div>
+          <p className="text3">6. User Control</p>
+          <p className="mt-2">
+            {`You have control over your data and can manage your preferences
+              directly through the extension's interface. If you wish to delete
+              your stored preferences, you can do so by clearing your browser's
+              data.`}
+          </p>
+        </div>
+        <div>
+          <p className="text3">7. Changes to This Privacy Policy</p>
+          <p className="mt-2">
+            We may update this Privacy Policy occasionally. We will notify you
+            of any changes by updating the effective date at the top of this
+            policy. Your continued use of the extension after any changes
+            indicates your acceptance of the new Privacy Policy.
+          </p>
+        </div>
+        <div>
+          <p className="text3">8. Contact Us</p>
+          <p className="mt-2">
+            If you have any questions or concerns regarding this Privacy Policy,
+            please contact us at <strong>mhshuvoalways@gmail.com</strong>
+          </p>
+        </div>
+      </div>
+    </MainComponent>
+  );
+};
+
+export default page;
